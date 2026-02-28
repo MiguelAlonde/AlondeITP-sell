36f7ee2f28fcfc44603c2cb7bf4f50132df7de2e
Miguel-BSIT2-2 <thomasmiguelalonde@gmail.com>
2026-02-28 11:16:31 +0800

    Update Program.cs

--- Alonde-sellitem/Program.cs
+++ Alonde-sellitem/Program.cs
@@ -1,36 +1,53 @@
-﻿namespace Alonde_sellitem
+﻿using System;
+
+namespace Alonde_sellitem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your full name");
-            string name = Console.ReadLine();
-            Console.WriteLine("Do you want to add items? y/n");
-            string choice = Console.ReadLine();
-            if (choice == "y")
-                Console.WriteLine("How many Items do you want to add? ");
-            int limit = int.Parse(Console.ReadLine());
+            string name = GetSellerName();
+            int limit = GetItemCount();
             string[] items = new string[limit];
             string[] values = new string[limit];
 
+            InputItems(items, values, limit);
+            DisplayInventory(name, items, values, limit);
+        }
+
+        static string GetSellerName()
+        {
+            Console.WriteLine("Enter your full name");
+            return Console.ReadLine();
+        }
+
+        static int GetItemCount()
+        {
+            Console.WriteLine("How many Items do you want to add? ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void InputItems(string[] items, string[] values, int limit)
+        {
             for (int i = 0; i < limit; i++)
             {
-
                 Console.WriteLine("Enter the Item: " + (i + 1));
                 items[i] = Console.ReadLine();
                 Console.WriteLine("Enter the Price: " + (i + 1));
                 values[i] = Console.ReadLine();
-
             }
+        }
+
+        static void DisplayInventory(string name, string[] items, string[] values, int limit)
+        {
+            Console.WriteLine(" ");
             Console.WriteLine("Inventory: ");
             Console.WriteLine("\n Seller: " + name);
             for (int i = 0; i < limit; i++)
             {
-                Console.WriteLine("item: " + (i + 1) + " : " + items[i]);
-                Console.WriteLine("price: " + (i + 1) + " : " + values[i]);
+                Console.WriteLine("item:  " + (i + 1) + "  : " + items[i]);
+                Console.WriteLine("price:  " + (i + 1) + "  : " + values[i]);
             }
         }
-
     }
 }
