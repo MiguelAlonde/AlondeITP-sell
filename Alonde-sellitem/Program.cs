01c9870c3ab424085bcc032d947bf8a71c030078
Reguyal-DCET <lourdlealcruz@gmail.com>
2026-02-28 10:40:15 +0800

    Initial commit

---
+++ Alonde-sellitem/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Alonde_sellitem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your full name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Do you want to add items? y/n");
+            string choice = Console.ReadLine();
+            if (choice == "y")
+                Console.WriteLine("How many Items do you want to add? ");
+            int limit = int.Parse(Console.ReadLine());
+            string[] items = new string[limit];
+            string[] values = new string[limit];
+
+            for (int i = 0; i < limit; i++)
+            {
+
+                Console.WriteLine("Enter the Item: " + (i + 1));
+                items[i] = Console.ReadLine();
+                Console.WriteLine("Enter the Price: " + (i + 1));
+                values[i] = Console.ReadLine();
+
+            }
+            Console.WriteLine("Inventory: ");
+            Console.WriteLine("\n Seller: " + name);
+            for (int i = 0; i < limit; i++)
+            {
+                Console.WriteLine("item: " + (i + 1) + " : " + items[i]);
+                Console.WriteLine("price: " + (i + 1) + " : " + values[i]);
+            }
+        }
+
+    }
+}
